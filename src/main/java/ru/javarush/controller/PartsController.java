5dc98074442f170c372e7a717b4e2b7400b3a2d9
vbugaenko <vicktorbugaenko@yandex.ru>
2018-09-19 14:22:15 +1000

    upd PartsController (add RequestParams)

--- src/main/java/ru/javarush/controller/PartsController.java
+++ src/main/java/ru/javarush/controller/PartsController.java
@@ -1,15 +1,14 @@
 package ru.javarush.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.javarush.db.entity.Part;
-import ru.javarush.service.PartsService;
 
 import java.util.List;
+import ru.javarush.service.PartsServiceImpl;
 
 /**
  * @author Victor Bugaenko
@@ -19,13 +18,22 @@ import java.util.List;
 @Controller
 public class PartsController
 {
-    PartsService partsService = new PartsService();
+    PartsServiceImpl partsService = new PartsServiceImpl();
 
     List<Part> parts = partsService.getAllParts();
 
     @RequestMapping(value="/", method = RequestMethod.GET)
     public String partsListWithFilters(
             @RequestParam(value = "sortBy",      required = false) String sortBy,
+            @RequestParam(value = "filter",      required = false) String filter,
+            @RequestParam(value = "page",        required = false) String page,
+            @RequestParam(value = "idForUpdate", required = false) String idForUpdate,
+            @RequestParam(value = "idForDelete", required = false) String idForDelete,
+            @RequestParam(value = "activateID",  required = false) String activateID,
+            @RequestParam(value = "editID",      required = false) String editID,
+            @RequestParam(value = "editTitle",   required = false) String editTitle,
+            @RequestParam(value = "editPrice",   required = false) String editPrice,
+            @RequestParam(value = "editAmount",  required = false) String editAmount,
             Model model )
     {
         model.addAttribute("parts",     parts   );

9cd725e1fb87fd156d6f0891727312bbc67d28c7
vbugaenko <vicktorbugaenko@yandex.ru>
2018-09-19 14:20:22 +1000

    add interface for PartsService

--- src/main/java/ru/javarush/service/PartsService.java
+++ src/main/java/ru/javarush/service/PartsService.java
@@ -1,23 +1,10 @@
 package ru.javarush.service;
 
-import ru.javarush.db.dao.PartsDAOImpl;
 import ru.javarush.db.entity.Part;
 
 import java.util.List;
 
-/**
- * Services
- *
- * @author Victor Bugaenko
- * @since 18.09.2018
- */
-
-public class PartsService
+public interface PartsService
 {
-    private PartsDAOImpl partsDAOimpl = new PartsDAOImpl();
-
-    public List<Part> getAllParts()
-    {
-        return partsDAOimpl.getAllParts();
-    }
+    List<Part> getAllParts();
 }
---
+++ src/main/java/ru/javarush/service/PartsServiceImpl.java
@@ -0,0 +1,24 @@
+package ru.javarush.service;
+
+import org.springframework.stereotype.Service;
+import ru.javarush.db.dao.PartsDAOImpl;
+import ru.javarush.db.entity.Part;
+
+import java.util.List;
+
+/**
+ * Services
+ *
+ * @author Victor Bugaenko
+ * @since 18.09.2018
+ */
+@Service
+public class PartsServiceImpl implements PartsService
+{
+    private PartsDAOImpl partsDAOimpl = new PartsDAOImpl();
+
+    public List<Part> getAllParts()
+    {
+        return partsDAOimpl.getAllParts();
+    }
+}
